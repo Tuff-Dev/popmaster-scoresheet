49b0906c4112c547e89c57f14fc26b62d3a07e63
Tuff-Dev <tuff2dev@gmail.com>
2020-10-02 13:35:04 +0100

    deploy config

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,5 @@ module.exports = {
       }
     }
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/popmaster-scoresheet/' : '/'
 }
